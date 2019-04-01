22dfe7cb11c7789fba7797d7b0933440e502cbd8
Soren's Coding Land <chaz.com@hotmail.co.uk>
2019-04-01 18:14:07 +0100

    Basic Pyramid shape and floor added & simple movement
    The movement needs to be developed to be have a complete 3D circular movement like an analogue stick would do.

---
+++ Spinning Top/Assets/Scripts/SpinnerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerMovement : MonoBehaviour
+{
+    [SerializeField] int Velocity = 1; // Maybe a max and min speed will be needed. int or bool?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementControls();
+    }
+
+    private void MovementControls()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Velocity * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Velocity * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Velocity * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Velocity * Time.deltaTime);
+        }
+    }
+}
